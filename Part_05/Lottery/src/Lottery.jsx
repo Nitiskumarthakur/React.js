5f49a31e3b1d683e4a3d164afb2c8ad36aca262f
Nitish Thakur <kumarnitish95651@gmail.com>
2025-12-18 22:43:04 +0530

    Lottery

--- Part_05/Lottery/src/Lottery.jsx
+++ Part_05/Lottery/src/Lottery.jsx
@@ -6,7 +6,7 @@ import Ticket from "./Ticket";
 export default function Lottery({n=3, winCondition}){
     let[ticket, setTicket] = useState(genTricket(n));
    // let isWinning = winCondition(ticket);
-    let isWinning = sum(ticket) === 15
+    let isWinning = sum(ticket) === 15;
     // console.log(isWin);
     // console.log(sum(ticket));
     
