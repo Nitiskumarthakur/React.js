ca07940edb44622ebe61dc956fa8515b1d092049
Nitish Thakur <kumarnitish95651@gmail.com>
2025-12-18 22:43:41 +0530

    gen && sum

--- Part_05/Lottery/src/Helper.js
+++ Part_05/Lottery/src/Helper.js
@@ -1,11 +1,11 @@
-function genTricket(n){
+function genTricket(n){ // Number langth n size like n=3.
     let arr = new Array(n);
     for(let i=0;i<n;i++){
         arr[i] = Math.floor(Math.random()*10);
     }
     return arr;
 }
-function sum(arr){
+function sum(arr){ // add using the reduce Method.
     return arr.reduce((sum, curr)=> sum + curr, 0);
 }
 
