1cb1a73c1e523e7de33e74807958050c4c3a4dc9
Nitish Thakur <kumarnitish95651@gmail.com>
2025-12-19 17:21:03 +0530

    yt

---
+++ Yt-Exploring/Todo_List/src/App.jsx
@@ -0,0 +1,14 @@
+ import DeleteIcon from '@mui/icons-material/Delete';
+
+//import './App.css'
+
+function App() {
+  return (
+    <div style={{display:"flex"}}>
+      
+      <h2>Todo List And Adding Time & Date</h2><DeleteIcon/> 
+    </div>
+  )
+}
+
+export default App
