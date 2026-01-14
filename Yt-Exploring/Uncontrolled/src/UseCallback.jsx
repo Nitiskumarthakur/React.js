30bbcd9bdd40cdb132c4081ab9a3b23d1fb6bc7b
Nitish Thakur <kumarnitish95651@gmail.com>
2026-01-14 18:30:18 +0530

    Uncontrolled

---
+++ Yt-Exploring/Uncontrolled/src/UseCallback.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import { Children } from "react";
+import { useCallback } from "react";
+
+
+export const Button = ({onClick, children})=>{
+    console.log(`Rendering Button ${children}`)
+
+    return(
+        <button onClick={onClick}>{children}</button>
+    )
+}
+export default function UseCallback() {
+    
+    const [count, setCount] =useState(0);
+     
+    //Memoize tht Increment Function.
+    const increment = useCallback(()=>{
+        console.log("increment inside");
+        setCount(count+1)
+    },[]);
+    //Memoize tht Decrement Function.
+    const decrement = useCallback(()=>{
+        console.log("decrement inside");
+        setCount(count-1);
+    },[]);
+
+    return (
+        <div>
+            <h2>Count: {count}</h2>
+            <Button onClick={increment}>Increment</Button>&nbsp;
+            <Button onClick={decrement}>Decrement</Button>
+        </div>
+    )
+}
